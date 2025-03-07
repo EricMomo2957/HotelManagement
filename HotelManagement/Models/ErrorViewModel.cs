a19c1a035ba6d499321b069f4e218897ad03b0a1
xx830 <xx830@CCSLAB530U46>
2025-03-07 12:08:30 +0800

    Add project files.

---
+++ HotelManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace HotelManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
+
